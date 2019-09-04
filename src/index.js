b857e888af06059f369eb99692bbdfc6370311ae
vitor <vitorpaulasantos@gmail.com>
2019-09-04 09:20:18 -0300

    referenciando os inputs e guardando em variaveis

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('funcionando')
+const $inputName = document.querySelector("#name-ciclic");
+const $inputMensalidade = document.querySelector("#mensalidade-ciclic");
+const $inputYear = document.querySelector("#year-ciclic");
+
+
+console.log($inputName.value);
+console.log($inputMensalidade.value);
+console.log($inputYear.value); // aqui ta com um erro que eu nao consigo mudar
