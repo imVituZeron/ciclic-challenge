105a0e24a07150ba2bc87187b60e21c655ce94d0
vitor <vitorpaulasantos@gmail.com>
2019-09-05 09:16:18 -0300

    criado o evento do button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const $inputName = document.querySelector("#name-ciclic");
 const $inputMensalidade = document.querySelector("#mensalidade-ciclic");
 const $inputYear = document.querySelector("#year-ciclic");
+const $btnSimular = document.querySelector(".button-ciclic");
+
+console.log('button = ', $btnSimular);
+console.log('name = ', $inputName);
+console.log('mensalidade =',$inputMensalidade);
 
 
-console.log($inputName.value);
-console.log($inputMensalidade.value);
-console.log($inputYear.value); // aqui ta com um erro que eu nao consigo mudar
+$btnSimular.addEventListener("click", ()=> {
+   console.log(`
+      quando clicar aqui devo fazer uma logica que vai passar o estado das variaveis
+      e ir para outra pagina`)
+
+});
