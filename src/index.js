2347e5862dcaaff2f52ff179383771ff2b192e24
vitor <vitorpaulasantos@gmail.com>
2019-09-06 16:11:31 -0300

    tentativa de fazer o logico de lucro do projeto

--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ console.log('button = ', $btnSimular);
 console.log('name = ', $inputName);
 console.log('mensalidade =',$inputMensalidade);
 
+// falta coisa pra arrumar essa logica de lucro;
+simuMoney = () => {
+   let total = ($inputMensalidade.value * ((( 1 + 0.517) * 24 - 1 ) / 0.517));
+   console.log(total);
+}
+
+
+
 
 $btnSimular.addEventListener("click", ()=> {
+   simuMoney();
    console.log(`
       quando clicar aqui devo fazer uma logica que vai passar o estado das variaveis
       e ir para outra pagina`)
